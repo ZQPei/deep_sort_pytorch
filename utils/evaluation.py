1d03f315425e644bef2cd6fe784805029b603fd6
masoud.mog <masoud.masoumi>
2020-05-19 13:12:29 +0430

    Added pedestrian detection on web server Reformatted some of codes

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -3,7 +3,7 @@ import numpy as np
 import copy
 import motmetrics as mm
 mm.lap.default_solver = 'lap'
-from utils.io_ import read_results, unzip_objs
+from utils.io import read_results, unzip_objs
 
 
 class Evaluator(object):
